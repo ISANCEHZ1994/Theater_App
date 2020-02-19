82783a5fc11c136dd60d61d07cc1cef4d6522e2a
Israel <Israel.pro1994@gmail.com>
2020-02-19 16:55:48 -0600

    created tables and some seed data

---
+++ theater_backend/db/migrate/20200219221503_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :description
+      t.string :mpaa
+      t.string :poster
+      t.string :released
+      t.string :trailer
+
+      t.timestamps
+    end
+  end
+end
--- theater_backend/db/seeds.rb
+++ theater_backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Movie.destroy_all
+Ticket.destroy_all
+
+
+israel = User.create({
+    name: 'Israel Sanchez',
+    email: 'Israel.pro1994@gmail.com',
+    password: '1994',
+    phone_number: 13
+})
+
+hero = Movie.create({
+    title: 'My Hero Academia The Movie Heros: Rising', 
+    description: "A group of youths, professional superheros in training, fight in a world full of people with powerful gifts called 'quirks'. Deku and his partners from the Academy face Nine, the strongest villain yet.",
+    mpaa: 'PG-13',
+    poster: 'https://vignette.wikia.nocookie.net/bokunoheroacademia/images/e/e9/Heroes_Rising_Promotional_Poster_2.png/revision/latest?cb=20190927203442',
+    released: 'FEB 26, 2020',
+    trailer: '_A-6qcgExA4'
+})
+
+Ticket.create({
+    movie_id: hero.id, user_id: israel.id
+})
