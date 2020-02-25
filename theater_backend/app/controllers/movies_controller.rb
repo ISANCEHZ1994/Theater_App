c4daf053b9c25b8f115ddc5484c814d47914df54
Israel <Israel.pro1994@gmail.com>
2020-02-25 16:56:05 -0600

    data appears at address

--- theater_backend/app/controllers/movies_controller.rb
+++ theater_backend/app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 class MoviesController < ApplicationController
+
     def index
         movies = Movie.all
+        render json: movies, :except => [:created_at, :updated_at]
     end
-
-    
 
 end
--- theater_backend/app/controllers/tickets_controller.rb
+++ theater_backend/app/controllers/tickets_controller.rb
@@ -1,5 +1,8 @@
 class TicketsController < ApplicationController
+
     def index
         tickets = Ticket.all
+        render json: tickets, :except => [:created_at, :updated_at]
     end
+
 end
